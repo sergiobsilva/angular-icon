5fa34272829cc5daebe25dd2437689295c933734
Vitaliy Bobrov <bobrov1989@gmail.com>
2017-03-18 21:01:15 +0100

    fix bugs

---
+++ demo/dist/angular-icon.component.d.ts
@@ -0,0 +1,11 @@
+export declare class IconComponent {
+    role: string;
+    ariaLabel: string;
+    iconClassName: boolean;
+    svgWidth: number;
+    svgHeight: number;
+    alt: string;
+    name: string;
+    size: number;
+    private _name;
+}
---
+++ demo/dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var demo_module_1 = require("./demo.module");
+core_1.enableProdMode();
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(demo_module_1.DemoModule);
+//# sourceMappingURL=main.js.map
