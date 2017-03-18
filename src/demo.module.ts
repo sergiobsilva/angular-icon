48e9a776d9f1fd9be53b6b7a475b32c9b03ed369
Vitaliy Bobrov <bobrov1989@gmail.com>
2017-03-18 20:42:12 +0100

    demo fix

--- src/demo.module.ts
+++ src/demo.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DemoComponent } from './demo.component';
 
-const IconModule = require('ngresizable').IconModule;
+const IconModule = require('angular-icon').IconModule;
 
 @NgModule({
   imports: [
